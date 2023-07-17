c1f5f41e024c41434225bb5a6f5dd18a0e2bf926
unknown <edgarerb@hotmail.es>
2023-07-17 16:35:55 +0200

    test 3

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,13 @@
+const string= 'esto es un texto'
+const number = 3748324732
+const array = ['test REACT', 'hola', 8, 28374367]
+const boolean = true
+const funcion = () => 1+1
+const objeto = { nombre: 'Edgar', edad: 32 }
+const fecha = new Date()
+
 export const PrimerComponente = () => {
   return (
-    <h1>PrimerComponente</h1>
+    <h1>{ string }</h1>
   )
 }
