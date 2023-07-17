4fea6b63b8b731e3751edb6d40293be263d57e83
unknown <edgarerb@hotmail.es>
2023-07-17 17:05:49 +0200

    asasa

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -8,6 +8,14 @@ const fecha = new Date()
 
 export const PrimerComponente = () => {
   return (
-    <h1>{ string }</h1>
+    <div>
+    <h1>Variables en JSX</h1>
+    <h4>variable tipo String:</h4><p>{string}</p>
+    <h4>variable tipo Number:</h4><p>{number}</p>
+    <h4>variable tipo array:</h4><p>{array}</p>
+    <h4>variable tipo boolean:</h4><p>{boolean}</p>
+    <h4>variable tipo funcion:</h4><p>{string}</p>
+    <h4>variable tipo String:</h4><p>{funcion()}</p>
+    </div>
   )
 }
