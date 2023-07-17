8bd5e563fad0ac3265aea4f60c20b451892eee06
unknown <edgarerb@hotmail.es>
2023-07-17 17:35:43 +0200

    bene

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,3 +1,5 @@
+import './PrimerComponente.css'
+
 const string= 'esto es un texto'
 const number = 3748324732
 const array = ['test REACT', 'hola', 8, 28374367]
@@ -6,9 +8,11 @@ const funcion = () => 1+1
 const objeto = { nombre: 'Edgar', edad: 32 }
 const fecha = new Date()
 
+
+
 export const PrimerComponente = () => {
   return (
-    <div>
+    <>
     <h1>Variables en JSX</h1>
     <h4>variable tipo String:</h4><p>{string}</p>
     <h4>variable tipo Number:</h4><p>{number}</p>
@@ -16,6 +20,6 @@ export const PrimerComponente = () => {
     <h4>variable tipo boolean:</h4><p>{boolean}</p>
     <h4>variable tipo funcion:</h4><p>{string}</p>
     <h4>variable tipo String:</h4><p>{funcion()}</p>
-    </div>
+    </>
   )
 }
