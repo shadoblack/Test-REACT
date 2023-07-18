7b9b22879cc0f05db66d9b67b29f601f4d83f9b3
unknown <edgarerb@hotmail.es>
2023-07-18 17:15:49 +0200

    prompts

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,25 +1,23 @@
-import './PrimerComponente.css'
+import PropTypes from 'prop-types'
 
-const string= 'esto es un texto'
-const number = 3748324732
-const array = ['test REACT', 'hola', 8, 28374367]
-const boolean = true
-const funcion = () => 1+1
-const objeto = { nombre: 'Edgar', edad: 32 }
-const fecha = new Date()
+export const PrimerComponente = ( {titulo, subtitulo} ) => {
 
 
-
-export const PrimerComponente = () => {
   return (
     <>
-    <h1>Variables en JSX</h1>
-    <h4>variable tipo String:</h4><p>{string}</p>
-    <h4>variable tipo Number:</h4><p>{number}</p>
-    <h4>variable tipo array:</h4><p>{array}</p>
-    <h4>variable tipo boolean:</h4><p>{boolean}</p>
-    <h4>variable tipo funcion:</h4><p>{string}</p>
-    <h4>variable tipo String:</h4><p>{funcion()}</p>
+    <h1>{titulo} </h1>
+    <h2>{subtitulo} </h2>
+    
     </>
   )
+}
+
+PrimerComponente.PropTypes ={
+  titulo: PropTypes.string.isRequired,
+  subtitulo: PropTypes.string.isRequired
+}
+
+PrimerComponente.defaultProps ={
+titulo: 'Test de REACT',
+subtitulo: 'Sección de Props'
 }
