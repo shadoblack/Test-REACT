dcfee0fe2b95d860bf720c38a818105c2d6034e1
unknown <edgarerb@hotmail.es>
2023-07-21 19:32:08 +0200

    custom hooks

--- src/components/UsersApp.jsx
+++ src/components/UsersApp.jsx
@@ -1,22 +1,16 @@
-import React, { useState} from "react"
+import React, { useState } from "react"
 import { UserList } from "./userList"
 
 export const UsersApp = () => {
-
-    const [endPoint, setendPoint] = useState('users')
-
-    const handleFetch = () =>{
-        setendPoint('comments')
+    const [endPoint, setEndPoint] = useState('users')
+    const handleFetch = () => {
+        setEndPoint('comments')
     }
-
-    
-    
-
-  return (
-<>
-<h1>Lista de usuarios</h1>
-<UserList endPoint={endPoint}></UserList>
-<button onClick={handleFetch}>Llamar a la API</button>
-</>
+    return (
+        <>
+            <h1>Lista de usuarios</h1>
+            <UserList endPoint={endPoint}></UserList>
+            <button onClick={handleFetch}>Llamar a la API</button>
+        </>
     )
 }
--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect,useState } from 'react'
+import React from 'react'
+import { useFetchData } from '../hooks/useFetchData'
 
-export const  UserList = ({ endPoint }) => {
+export const UserList = ({ endPoint }) => {
 
-    const [data, setData] = useState([])
-
-    const fetchdata = async () => {
-        try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`)
-            const data = await response.json()
-            setData(data)
-        } catch (error) {
-            console.error(error)
-
-        }
-
-    }
-
-    useEffect(() => {
-        fetchdata()
-    }, [endPoint])
+    const { data, isLoading } = useFetchData(endPoint)
     return (
+        <>
 
-
-        <ul>
-            {endPoint == 'users' ?  data.map(item => <li key={item.id}>Name: {item.name} Email: {item.email}</li>) 
-                                : data.map(item => <li key={item.id}>Name: {item.name} Email: {item.body}</li>)}
-                                </ul>
+            <ul>
+                {isLoading ? <p>cargando...</p>
+                    : endPoint == 'users'
+                        ? data.map(item => <li key={item.id}>Name: {item.name} Email: {item.email}</li>)
+                        : data.map(item => <li key={item.id}>Name: {item.name} Email: {item.body}</li>)
+                        }
+            </ul>
+        </>
     )
 }
